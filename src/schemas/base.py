655669dcd96aedfe7fdbeb802d87a3aff2a4189b
tatsuki.onoda <tatsuki.onoda@arsaga.jp>
2023-12-22 16:48:51 +0900

    edit schmers field using pydantic alias generator

--- src/schemas/base.py
+++ src/schemas/base.py
@@ -2,19 +2,18 @@
 from datetime import datetime
 from uuid import UUID
 
-from humps import camel
-from pydantic import BaseModel, ConfigDict
-
-# スネークケースをキャメルケースへ変換
-def to_camel(string: str) -> str:
-    return camel.case(string)
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    alias_generators
+)
 
 
 class BaseRequestModel(BaseModel):
     model_config = ConfigDict(
         extra="ignore",  # 定義していないフィールドの無視
         frozen=True,  # フィールド値の変更を許可しない
-        alias_generator=to_camel,  # 各フィールドにキャメルケースのエイリアスを作成
+        alias_generator=alias_generators.to_camel,  # 各フィールドにキャメルケースのエイリアスを作成
         allow_population_by_field_name=True,  # キャラメルケースで送られてきたリクエストも許可する
     )
 
